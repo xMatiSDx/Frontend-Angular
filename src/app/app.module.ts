f551913e287ec808d814e16ce83fe10048a2cc41
xMatiSDx <maty_arias@hotmail.com>
2022-10-01 00:01:55 -0300

    header ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     NgbModule,
-    FontAwesomeModule
+    NgbCollapseModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, Directive } from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faGithub } from  '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faSignIn } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 
 @Component({
@@ -11,17 +12,40 @@ import { faSignIn } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
+
 export class HeaderComponent implements OnInit {
+  
+  @Directive({selector: '[ngbCollapse]', exportAs: 'ngbCollapse'})
+
+  @Input('ngbCollapse')
+
+  public isCollapsed : boolean = true;
+
+  set collapsed(isCollapsed: boolean) {
+    if (this.isCollapsed !== isCollapsed) {
+      this.isCollapsed = isCollapsed;
+    }
+  }
+
+  @Output() ngbCollapseChange = new EventEmitter<boolean>();
 
   faBars = faBars;
   faTwitter = faTwitter;
   faGithub = faGithub;
   faInstagram = faInstagram
   faSignIn = faSignIn
+  faLinkedIn = faLinkedin
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit(): void {
+  }
+
+  toggle(open: boolean = this.isCollapsed) {
+    this.collapsed = !open;
+    this.ngbCollapseChange.next(this.isCollapsed);
   }
 
 }
